4c8d143e8e234c7dd34df93703930a13e72ed765
ravi <artiveloper@gmail.com>
2020-11-02 17:34:50 +0900

    [프로젝트 환경설정]

---
+++ src/main/java/study/querydsl/Hello.java
@@ -0,0 +1,16 @@
+package study.querydsl;
+
+import lombok.Getter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+public class Hello {
+
+    @Id @GeneratedValue
+    private Long id;
+
+}
