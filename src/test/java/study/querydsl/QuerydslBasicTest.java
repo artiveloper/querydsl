16e0085dd03e767df17cb51bda224113e4afbc92
ravi <artiveloper@gmail.com>
2020-11-04 17:37:09 +0900

    [중급 문법]
    SQL Function 호출하기.

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -576,4 +576,17 @@ public class QuerydslBasicTest {
                 .execute();
     }
 
+    @Test
+    void sqlFunction() {
+        List<String> result = queryFactory
+                .select(Expressions.stringTemplate(
+                        "function('replace', {0}, {1}, {2})",
+                        member.username, "member", "M"))
+                .from(member)
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
 }
